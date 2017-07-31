09ab46492d2f4e3230e48408bdf426094a3f1595
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-08-01 00:09:05 +0300

    decompose Bookshelf to a separate component

---
+++ src/Bookshelf.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Bookshelf extends Component {
+  static PropTypes = {
+    books: PropTypes.array.isRequired
+  }
+
+  state = {
+    title: {
+      currentlyReading: 'Currently Reading',
+      wantToRead: 'Want to Read',
+      read: 'Read'
+    }
+  }
+
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.state.title[this.props.type]}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.filter((book) => {
+                return book.shelf === this.props.type;
+              }).map((book) => (
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                      <div className="book-shelf-changer">
+                        <select>
+                          <option value="none" disabled>Move to...</option>
+                          <option value="currentlyReading" checked>Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.authors.map((author) => <p key={author}>{author}</p>)}</div>
+                </div>
+                </li>
+              ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Bookshelf;
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,110 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Bookshelf from './Bookshelf';
 
-function ListBooks (props) {
-  return (
-    <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
-      <div className="list-books-content">
-        <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.books
-                  .filter((book) => book.shelf === 'currentlyReading')
-                  .map((book) => (
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading" checked>Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors.map((author) => <p key={author}>{author}</p>)}</div>
-                    </div>
-                    </li>
-                  ))}
-              </ol>
-            </div>
-          </div>
-
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.books
-                  .filter((book) => book.shelf === 'wantToRead')
-                  .map((book) => (
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading" checked>Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors.map((author) => <p key={author}>{author}</p>)}</div>
-                    </div>
-                    </li>
-                  ))}
-              </ol>
-            </div>
-          </div>
-
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.books
-                  .filter((book) => book.shelf === 'read')
-                  .map((book) => (
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading" checked>Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors.map((author) => <p key={author}>{author}</p>)}</div>
-                    </div>
-                    </li>
-                  ))}
-              </ol>
-            </div>
+class ListBooks extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Bookshelf
+              type="currentlyReading"
+              books={this.props.books}
+            />
+            <Bookshelf
+              type="wantToRead"
+              books={this.props.books}
+            />
+            <Bookshelf
+              type="read"
+              books={this.props.books}
+            />
           </div>
         </div>
+        <div className="open-search">
+          <Link to='/search'>Add a book</Link>
+        </div>
       </div>
-      <div className="open-search">
-        <Link to='/search'>Add a book</Link>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default ListBooks;
