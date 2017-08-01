28c84d0aa160789d7a8424aea98784a24910020d
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-08-02 00:28:43 +0300

    feat: add book searching.
    It needs to be refactored. Search page should render ordered list of
    books instead of set of bookshelves.

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -10,23 +10,33 @@ class Bookshelf extends Component {
     title: {
       currentlyReading: 'Currently Reading',
       wantToRead: 'Want to Read',
-      read: 'Read'
+      read: 'Read',
+      none: 'Other'
     }
   }
 
   render() {
+    const filteredBooks = this.props.books.filter((book) => {
+      return book.shelf === this.props.type;
+    });
+
+    if (!filteredBooks.length) {
+      return null;
+    }
+
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.state.title[this.props.type]}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {this.props.books.filter((book) => {
-                return book.shelf === this.props.type;
-              }).map((book) => (
+            {filteredBooks.map((book) => (
                 <li key={book.id}>
                   <div className="book">
                     <div className="book-top">
+
+                      {book.imageLinks && book.imageLinks.thumbnail ?
                       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                      : null}
                       <div className="book-shelf-changer">
                         <select>
                           <option value="none" disabled>Move to...</option>
@@ -38,7 +48,7 @@ class Bookshelf extends Component {
                       </div>
                   </div>
                   <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors.map((author) => <p key={author}>{author}</p>)}</div>
+                  <div className="book-authors">{book.authors ? book.authors.map((author) => <p key={author}>{author}</p>) : null}</div>
                 </div>
                 </li>
               ))}
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,32 +1,67 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import Bookshelf from './Bookshelf';
 
-function SearchBooks (props) {
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <Link
-          to='/'
-          className='close-search'
-        >Close</Link>
-        <div className="search-books-input-wrapper">
-          {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class SearchBooks extends Component {
+  state = {
+    books: []
+  }
 
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-          <input type="text" placeholder="Search by title or author"/>
+  updateQuery = (query) => {
+    BooksAPI.search(query, 10).then((books) => {
+      this.setState({
+        books: books.error ? [] : books
+      });
+    });
+  }
 
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link
+            to='/'
+            className='close-search'
+          >Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+            <Bookshelf
+              type="currentlyReading"
+              books={this.state.books}
+            />
+            <Bookshelf
+              type="wantToRead"
+              books={this.state.books}
+            />
+            <Bookshelf
+              type="read"
+              books={this.state.books}
+            />
+            <Bookshelf
+              type="none"
+              books={this.state.books}
+            />
+          <ol className="books-grid"></ol>
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid"></ol>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default SearchBooks;
