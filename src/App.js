108c5219255d33a3af9253ffe0541129bc6768d4
Ruslan Posevkin <rus.posevkin@gmail.com>
2017-08-06 00:19:26 +0300

    refactor: remove unnecessary ListBooks component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
-import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
+import Bookshelf from './Bookshelf';
 import './App.css';
 
 class BooksApp extends React.Component {
@@ -22,9 +23,30 @@ class BooksApp extends React.Component {
       <div className="app">
         <Switch>
           <Route exact path='/' render={() => (
-            <ListBooks
-              books={this.state.books}
-            />
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <Bookshelf
+                    type="currentlyReading"
+                    books={this.state.books}
+                  />
+                  <Bookshelf
+                    type="wantToRead"
+                    books={this.state.books}
+                  />
+                  <Bookshelf
+                    type="read"
+                    books={this.state.books}
+                  />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+              </div>
+            </div>
           )}/>
           <Route path='/search' component={SearchBooks}/>
         </Switch>
